2a316d3fbf1de5b9fb7784957deb840fa18f4fd8
Paul Goetze <paul.christoph.goetze@gmail.com>
2014-12-11 22:41:55 +0100

    Add courses-not-found error

--- lib/daigaku/exceptions.rb
+++ lib/daigaku/exceptions.rb
@@ -1,10 +1,12 @@
 module Daigaku
-  class Error < StandardError; end
 
+  class Error < StandardError; end
+  class CourseNotFoundError < Error; end
   class ChaptersNotFoundError < Error; end
   class UnitsNotFoundError < Error; end
   class TaskNotFoundError < Error; end
   class ReferenceSolutionNotFoundError; end
   class SolutionNotFoundError < Error; end
   class ScaffoldError < Error; end
+
 end
