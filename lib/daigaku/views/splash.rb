eb0fb50a7f795134c298a0ed9e3e9e055c8874e8
Paul Goetze <paul.christoph.goetze@gmail.com>
2015-01-22 21:26:40 +0100

    Change splash to be rubyish
    * Background: red
    * font color: white & bold

--- lib/daigaku/views/splash.rb
+++ lib/daigaku/views/splash.rb
@@ -5,34 +5,45 @@ module Daigaku
       include Views
 
       def initialize
+        start_color
+        init_pair(COLOR_RED, COLOR_WHITE, COLOR_RED)
+
         title = "DAIGAKU"
         subtitle = "Learning the Ruby programming language dead easy."
 
         panel = default_window
-        panel.setpos((lines / 4), (cols - title.length) / 2)
-        panel.addstr(title)
-        panel.refresh
 
-        sleep 0.5
+        panel.attron(color_pair(COLOR_RED) | A_BOLD) do
+          lines.times do |line|
+            panel.setpos(line, 0)
+            panel << ' ' * cols
+          end
 
-        ruby_ascii_art.each_with_index do |line, index|
-          panel.setpos(lines / 4 + 2 + index, (cols - line.length) / 2)
-          panel.addstr line
-          sleep 0.06
+          panel.setpos((lines / 4), (cols - title.length) / 2)
+          panel << title
           panel.refresh
+
+          sleep 0.5
+
+          ruby_ascii_art.each_with_index do |line, index|
+            panel.setpos(lines / 4 + 2 + index, (cols - line.length) / 2)
+            panel << line
+            sleep 0.06
+            panel.refresh
+          end
+
+          panel.setpos(lines / 4 + 11, (cols - subtitle.length) / 2)
+
+          subtitle.chars do |char|
+            panel << char
+            panel.refresh
+            sleep 0.02
+          end
+
+          sleep 2.5
+
+          close_screen
         end
-
-        panel.setpos(lines / 4 + 11, (cols - subtitle.length) / 2)
-
-        subtitle.chars do |char|
-          panel.addch char
-          panel.refresh
-          sleep 0.05
-        end
-
-        sleep 2
-
-        close_screen
       end
 
       def ruby_ascii_art
@@ -42,8 +53,8 @@ module Daigaku
             "/___\\/___\\/___\\",
             " \\  \\  . / . / ",
             "   \\ \\ ./ ./   ",
-            "    \\\\/ /     ",
-            "      \\/      "
+            "    \\\\ / /    ",
+            "      \\./     "
         ]
       end
     end
