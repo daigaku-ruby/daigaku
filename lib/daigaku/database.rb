a9ef323066d11db01677375f0e6a5d1a748bd0a7
Paul Goetze <paul.christoph.goetze@gmail.com>
2015-01-30 00:18:59 +0100

    Add write & read functionality to database

--- lib/daigaku/database.rb
+++ lib/daigaku/database.rb
@@ -3,14 +3,32 @@ require 'fileutils'
 
 module Daigaku
 
-  class Database < YAML::Store
+  class Database
     include Singleton
 
     def initialize
       db_file = Daigaku.config.storage_file
-      FileUtils.makedirs(File.dirname(db_file))
+      directory = File.dirname(db_file)
+      FileUtils.makedirs(directory) unless Dir.exist?(directory)
 
-      @db = super(db_file)
+      @db =  YAML::Store.new(db_file)
+    end
+
+    # Defines getter and setter methods for arbitrarily named methods.
+    # @xample
+    #   Diagaku::Database.instance.answer = 42
+    #   => saves 'anser: 42' to database
+    #
+    #   Daigaku::Database.instance.answer
+    #   => 42
+    def method_missing(method, *args, &block)
+      if method =~ /.*=$/
+        @db.transaction { @db[method.to_s.gsub(/=$/, '')] = args[0] }
+      elsif args.count == 0
+        @db.transaction { @db[method.to_s] }
+      else
+        super
+      end
     end
   end
 
--- spec/daigaku/database_spec.rb
+++ spec/daigaku/database_spec.rb
@@ -8,5 +8,27 @@ describe Daigaku::Database do
     Daigaku::config.instance_variable_set(:@storage_file, local_storage_file)
   end
 
-  it { is_expected.to be_a YAML::Store }
+  it "creates the database file(s) in the .daigaku directory on access" do
+    Daigaku::Database.instance.call_an_arbitrary_method
+    expect(File.exist?(local_storage_file)).to be_truthy
+  end
+
+  it "allows setting arbitrary keys by setter methods" do
+    expect { Daigaku::Database.instance.ise_cream = "vanilla choco" }
+      .not_to raise_error
+  end
+
+  it "allows getting subsequently set keys" do
+    Daigaku::Database.instance.sushi = "sushi"
+    expect(Daigaku::Database.instance.sushi).to eq "sushi"
+  end
+
+  it "returns nil for not set keys" do
+    expect(Daigaku::Database.instance.hamburger).to be_nil
+  end
+
+  it "raises an method missing errror for non getter/setter methods" do
+    expect { Daigaku::Database.instance.arbitrary_method(1, 2) }
+      .to raise_error NoMethodError
+  end
 end
