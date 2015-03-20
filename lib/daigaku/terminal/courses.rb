7b7e7795c8abcecdefd118fdc60bf9bf79339cac
Paul Goetze <paul.christoph.goetze@gmail.com>
2015-03-20 22:03:26 +0100

    Remove branch name from github course dir on unzip
    * by default github the base folder in a github repo master.zip file has a '...-master' suffix which is not wanted to be in the courses name

--- lib/daigaku/terminal/courses.rb
+++ lib/daigaku/terminal/courses.rb
@@ -21,10 +21,12 @@ module Daigaku
                     desc: 'Download Github repository'
       desc 'courses download [URL] [OPTIONS]', 'Download a new daigaku course from [URL]'
       def download(url = nil)
-        url = github_repo(Daigaku.config.initial_course) unless url
+        use_initial_course = url.nil? && options[:github].nil?
+        url = github_repo(Daigaku.config.initial_course) if use_initial_course
         url = github_repo(options[:github]) if options[:github]
 
         url_given = (url =~ /\A#{URI::regexp(['http', 'https'])}\z/)
+        github = use_initial_course || options[:github] || url.match(/github\.com/)
 
         raise Download::NoUrlError unless url_given
         raise Download::NoZipFileUrlError unless File.basename(url) =~ /\.zip/
@@ -35,21 +37,16 @@ module Daigaku
         file_name = File.join(courses_path, url.split('/').last)
 
         File.open(file_name, 'w') { |file| file << open(url).read }
-        unzip(file_name)
-      rescue Download::NoUrlError => e
-        say_warning "\"#{url}\" is not a valid URL!"
-      rescue Download::NoZipFileUrlError => e
-        say_warning "\"#{url}\" is not a URL of a *.zip file!"
-      rescue Exception => e
-        message = [
-          "Error while downloading course from URL",
-          "\"#{url}\"\n",
-          "#{e.message}"
-        ].join("\n")
+        course_name = unzip(file_name, github)
 
-        say_warning message
+        say_info "Successfully downloaded the course \"#{course_name}\"!"
+      rescue Download::NoUrlError => e
+        print_download_warning(url, "\"#{url}\" is not a valid URL!")
+      rescue Download::NoZipFileUrlError => e
+        print_download_warning(url, "\"#{url}\" is not a URL of a *.zip file!")
+      rescue Exception => e
+        print_download_warning(url, e.message)
       ensure
-        say_info Terminal.text :hint_course_download
         FileUtils.rm(file_name) if File.exist?(file_name.to_s)
       end
 
@@ -68,20 +65,41 @@ module Daigaku
         "#{text}\n#{Terminal.text :hint_course_download}"
       end
 
-      def unzip(file_path)
+      def unzip(file_path, github = false)
         target_dir = File.dirname(file_path)
+        course_name = nil
 
         Zip::File.open(file_path) do |zip_file|
           zip_file.each do |entry|
-            zip_file.extract(entry, "#{target_dir}/#{entry}") { true }
+
+            if github
+              first, *others = entry.to_s.split('/')
+              directory = File.join(first.split('-')[0..-2].join('-'), others)
+            else
+              directory = entry.to_s
+            end
+
+            course_name ||= directory.split('/').first.gsub(/_+/, ' ')
+            zip_file.extract(entry, "#{target_dir}/#{directory}") { true }
           end
         end
 
         FileUtils.rm(file_path)
+        course_name
       end
 
       def github_repo(user_and_repo)
         "https://github.com/#{user_and_repo}/archive/master.zip"
+      end
+
+      def print_download_warning(url, text)
+        message = [
+          "Error while downloading course from URL \"#{url}\"",
+          "#{text}\n",
+          Terminal.text(:hint_course_download)
+        ].join("\n")
+
+        say_warning message
       end
     end
 
